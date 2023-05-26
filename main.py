69ca2ac461e9a557326483ab1e3522994225fe7a
Aline Pillot <acharron55@gmail.com>
2023-05-26 23:26:15 +0200

    test2

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def read_id(customer_id: int):
     df = pd.read_csv('df_sample.csv', index_col=0, header=0)
     return df.shape
     # Choisir le bon client
-    df = df.loc[df['SK_ID_CURR'] == customer_id]
-    feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-    X = df[feats]
+    #df = df.loc[df['SK_ID_CURR'] == customer_id]
+    #feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    #X = df[feats]
     # importer le modèle
     #model_uri = "file:///C:/Users/LN6428/Documents/P7/OC-P7/mlruns/222107729629896078/f374711e229f4188a406d396f2080269/artifacts/model"
-    model_uri = "./model"
-    loaded_model = mlflow.sklearn.load_model(model_uri)
+    #model_uri = "./model"
+    #loaded_model = mlflow.sklearn.load_model(model_uri)
     # Calculer le score
-    score = loaded_model.predict_proba(X)
-    response = score[0][1]
-    return response
+    #score = loaded_model.predict_proba(X)
+    #response = score[0][1]
+    #return response
